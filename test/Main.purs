24bac907483692fad7b92d8cba37c8cb53996e52
hrb90 <harrisonrbrown90@gmail.com>
2017-12-10 23:58:07 -0500

    Use

--- test/Main.purs
+++ test/Main.purs
@@ -6,8 +6,7 @@ import Control.Monad.Eff.Console (log)
 import Data.Group (class Group, ginverse)
 import Data.Group.Free (Free(..), Signed(..))
 import Data.List (List)
-import Data.Monoid (class Monoid, (<>), mempty)
-import Data.Semigroup (class Semigroup)
+import Data.Monoid (class Monoid, mempty)
 import Data.Tuple (Tuple(..))
 import Test.QuickCheck (class Arbitrary, QC, arbitrary, quickCheck')
 import Test.QuickCheck.Gen (Gen)
@@ -15,6 +14,26 @@ import Test.QuickCheck.Laws (A, checkLaws)
 import Test.QuickCheck.Laws.Data.Monoid (checkMonoid)
 import Test.QuickCheck.Laws.Data.Semigroup (checkSemigroup)
 import Type.Proxy (Proxy(..))
+
+newtype F a = F (Free a)
+
+instance arbitraryF :: Arbitrary a => Arbitrary (F a) where
+  arbitrary = mapArb <$> arbitrary' where
+    arbitrary' :: Gen (List (Tuple a Boolean))
+    arbitrary' = arbitrary
+
+    mapSigned (Tuple x true)  = Positive x
+    mapSigned (Tuple x false) = Negative x
+
+    mapArb = F <<< Free <<< (map mapSigned)
+
+derive newtype instance eqF :: Eq a => Eq (F a)
+
+derive newtype instance semigroupF :: Eq a => Semigroup (F a)
+
+derive newtype instance monoidF :: Eq a => Monoid (F a)
+
+derive newtype instance groupF :: Eq a => Group (F a)
 
 checkGroup :: forall eff g
               . Group g
@@ -35,29 +54,6 @@ checkGroup _ = do
   
   rightCancel :: g -> Boolean
   rightCancel x = x <> (ginverse x) == mempty
-
-newtype F a = F (Free a)
-
-instance arbitraryF :: Arbitrary a => Arbitrary (F a) where
-  arbitrary = mapArb <$> arbitrary' where
-    arbitrary' :: Gen (List (Tuple a Boolean))
-    arbitrary' = arbitrary
-
-    mapSigned (Tuple x true)  = Positive x
-    mapSigned (Tuple x false) = Negative x
-
-    mapArb = F <<< Free <<< (map mapSigned)
-
-derive instance eqF :: Eq a => Eq (F a)
-
-instance semigroupF :: Eq a => Semigroup (F a) where
-  append (F x) (F y) = F $ append x y
-
-instance monoidF :: Eq a => Monoid (F a) where
-  mempty = F $ mempty
-
-instance groupF :: Eq a => Group (F a) where
-  ginverse (F x) = F $ ginverse x
 
 main :: forall eff. QC eff Unit
 main = checkLaws "Free group" do
